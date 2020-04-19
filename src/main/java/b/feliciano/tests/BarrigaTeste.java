ee927bcbbc94a41129867baadd23e05d5db4c75c
Beatriz Feliciano <bialetobf@gmail.com>
2020-04-19 12:58:27 -0300

    CT002

--- src/main/java/b/feliciano/tests/BarrigaTeste.java
+++ src/main/java/b/feliciano/tests/BarrigaTeste.java
@@ -2,6 +2,9 @@ package b.feliciano.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import b.feliciano.core.BaseTest;
@@ -17,4 +20,34 @@ public class BarrigaTeste extends BaseTest{
 			.statusCode(401)
 		;
 	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login= new HashMap<String, String>();
+		login.put("email", "fulaninho@123");
+		login.put("senha", "123456");
+		
+		String token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+	
+		
+		Map<String, String> conta = new HashMap<String, String>();
+		conta.put("nome", "novaconta1");
+		
+		given()
+			.header("Authorization","JWT "+ token)
+			.body(conta)
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201);
+		;
+	}
 }
+
