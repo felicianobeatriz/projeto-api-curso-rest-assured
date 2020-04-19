7477e6084cb7e3c03d488724be3a1c45d37b7219
Beatriz Feliciano <bialetobf@gmail.com>
2020-04-19 14:00:50 -0300

    CT003

--- src/main/java/b/feliciano/tests/BarrigaTeste.java
+++ src/main/java/b/feliciano/tests/BarrigaTeste.java
@@ -5,14 +5,35 @@ import static io.restassured.RestAssured.given;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import b.feliciano.core.BaseTest;
 
 public class BarrigaTeste extends BaseTest{
+	
+	private String TOKEN;
 
+	@Before
+	public void efeturarLogin() {
+		Map<String, String> login= new HashMap<String, String>();
+		login.put("email", "fulaninho@123");
+		login.put("senha", "123456");
+		
+		TOKEN = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+
+	}
+	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
+		
 		given()
 		.when()
 			.get("/contas")
@@ -23,30 +44,35 @@ public class BarrigaTeste extends BaseTest{
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login= new HashMap<String, String>();
-		login.put("email", "fulaninho@123");
-		login.put("senha", "123456");
-		
-		String token = given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token")
-		;
 	
-		
 		Map<String, String> conta = new HashMap<String, String>();
 		conta.put("nome", "novaconta1");
 		
 		given()
-			.header("Authorization","JWT "+ token)
+			.header("Authorization","JWT "+ TOKEN)
 			.body(conta)
 		.when()
 			.post("/contas")
 		.then()
 			.statusCode(201);
+		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		
+	
+		
+		Map<String, String> conta = new HashMap<String, String>();
+		conta.put("nome", "Conta Alterada!");
+		
+		given()
+			.header("Authorization","JWT "+ TOKEN)
+			.body(conta)
+		.when()
+			.put("/contas/105806")
+		.then()
+			.statusCode(200);
 		;
 	}
 }
